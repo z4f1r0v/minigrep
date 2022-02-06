210ee064e47713009a4559c0bc83e7eb7d959ba9
Alexander Zafirov <alzaf@leo-pharma.com>
2022-02-06 22:08:30 +0100

    Refactoring getting crowded

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-use std::env;
+extern crate core;
+
+use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1)
+        });
+
+    let query = config.query;
+    let filename = config.filename;
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("Contents are {}", contents);
+
+    Ok(())
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+impl<'a> Config<'a> {
+    fn new(args: &'a [String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = &args[1];
+        let filename = &args[2];
+
+        Ok(Config { query, filename })
+    }
+}
+
