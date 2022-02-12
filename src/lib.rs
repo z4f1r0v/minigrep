0f49054fb2e63662ff327b0c4af5cb147c381288
Alexander Zafirov <alzaf@leo-pharma.com>
2022-02-12 12:56:15 +0100

    Move case-sensitive to ignore-case

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs;
 
-use crate::config::{count_lines, search, search_case_sensitive};
+use crate::config::{count_lines, search_case_insensitive, search_case_sensitive};
 pub use crate::config::Config;
 
 mod config;
@@ -12,8 +12,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     if config.count_lines_only {
         let result = count_lines(config.query, &contents);
         println!("{}", result)
-    } else if config.case_sensitive {
-        let results = search(config.query, &contents);
+    } else if config.ignore_case {
+        let results = search_case_insensitive(config.query, &contents);
         results.iter().for_each(|x| println!("{}", x));
     } else {
         let results = search_case_sensitive(config.query, &contents);
