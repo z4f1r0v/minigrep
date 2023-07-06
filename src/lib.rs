01218d9e81921f891d1c05d4c6282197411274e2
Aλexander Zafirov <alexander@zafirov.me>
2023-07-06 10:26:51 +0200

    Add colours (#2)
    * Simplify search functions
    
    * Color sought terms
    
    * tst
    
    * Refactoring things
    
    * Implemented case insensitive query matching with colour highlighting
    
    * Update to latest clap
    
    ---------
    
    Co-authored-by: Alexander Zafirov <alzaf@leo-pharma.com>
    Co-authored-by: Alexander Zafirov <aza@logpoint.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,91 @@
-use std::error::Error;
-use std::fs;
+use std::{error::Error, fs};
 
-use crate::config::{count_lines, search_case_insensitive, search_case_sensitive};
-pub use crate::config::Config;
+use clap::ArgMatches;
+use regex::Regex;
 
-mod config;
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub filename: &'a str,
+    pub count_lines_only: bool,
+    pub case_insensitive: bool,
+}
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+impl<'a> Config<'a> {
+    pub fn parse(matches: &'a ArgMatches) -> Result<Config<'a>, &'a str> {
+        let count_lines_only = matches.get_flag("count");
+        let case_insensitive = matches.get_flag("case_insensitive");
+        let query = matches.get_one::<String>("QUERY").expect("Missing query.");
+        let filename = matches.get_one::<String>("FILENAME").expect("Missing filename.");
 
-    if config.count_lines_only {
-        let result = count_lines(config.query, &contents);
-        println!("{}", result)
-    } else if config.ignore_case {
-        let results = search_case_insensitive(config.query, &contents);
-        results.iter().for_each(|x| println!("{}", x));
+        Ok(Config {
+            query,
+            filename,
+            count_lines_only,
+            case_insensitive,
+        })
+    }
+
+    pub fn new(config: Config) -> Result<(), Box<dyn Error>> {
+        let contents = fs::read_to_string(config.filename)?;
+        let results = filter_lines(contents, config.case_insensitive, config.query);
+
+        if config.count_lines_only {
+            println!("{}", results.len());
+        } else {
+            results.iter().for_each(|x| println!("{}", x));
+        };
+
+        Ok(())
+    }
+}
+
+pub fn filter_lines(contents: String, case_insensitive: bool, query: &str) -> Vec<String> {
+    let regex_pattern = if case_insensitive {
+        format!(r"(?i){}", regex::escape(query))
     } else {
-        let results = search_case_sensitive(config.query, &contents);
-        results.iter().for_each(|x| println!("{}", x));
+        format!(r"{}", regex::escape(query))
     };
+    let regex = Regex::new(&regex_pattern).expect("Invalid regex");
+    let red_highlight = "\x1b[31m$0\x1b[0m"; // ANSI escape code for red color
 
-    Ok(())
+    contents
+        .lines()
+        .filter(|line| regex.is_match(line))
+        .map(|line| regex.replace_all(line, red_highlight).to_string())
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, pro\u{1b}[31mduct\u{1b}[0mive."],
+            filter_lines(contents.to_string(), false, query)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["\u{1b}[31mRust\u{1b}[0m:", "T\u{1b}[31mrust\u{1b}[0m me."],
+            filter_lines(contents.to_string(), true, query)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,46 @@ extern crate core;
 
 use std::process;
 
-use clap::{App, Arg, arg, ArgMatches};
+use clap::{Arg, ArgMatches, ArgAction, Command};
 
 use picogrep::Config;
 
 fn main() {
-    let matches: ArgMatches = App::new("picogrep")
+    let matches: ArgMatches = Command::new("picogrep")
         .about("A miniature version of grep")
-        .arg(
-            Arg::new("ignore-case")
-                .short('i')
-                .long("ignore-case")
-                .help("Ignore case sensitivity")
-                .takes_value(false)
-        )
         .arg(
             Arg::new("count")
                 .short('c')
                 .long("count")
                 .help("Return the amount matches")
-                .takes_value(false)
+                .action(ArgAction::SetTrue)
         )
-        .arg(arg!([QUERY]))
-        .arg(arg!([FILENAME]))
+        .arg(
+            Arg::new("case_insensitive")
+                .short('i')
+                .long("case-insensitive")
+                .help("Perform a case insensitive search")
+                .action(ArgAction::SetTrue)
+        )
+        .arg(
+            Arg::new("QUERY")
+                .help("The string to search for")
+                .required(true)
+        )
+        .arg(
+            Arg::new("FILENAME")
+                .help("The file to search")
+                .required(true)
+        )
         .get_matches();
 
-    let config = Config::new(&matches)
+    let config = Config::parse(&matches)
         .unwrap_or_else(|err| {
             eprintln!("Problem parsing arguments: {}", err);
             process::exit(1)
         });
 
-    if let Err(e) = picogrep::run(config) {
+    if let Err(e) = Config::new(config) {
         eprintln!("Application error: {}", e);
         process::exit(1)
     }
